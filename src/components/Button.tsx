3293d2fe73870909b087ced674d8aa46bd2968f0
Ayngie <angelicareutersward@gmail.com>
2023-05-18 16:56:15 +0200

    :sparkles: (feat): created basic layout and styling for buttons and counter

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,25 @@
+import styled from "styled-components";
+
 interface IButtonProps {
   symbol: string;
   text: string;
 }
+
+const StyledButton = styled.button`
+  padding: 1rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  border-radius: 50px;
+  border-style: none;
+`;
+
 export const Button = ({ symbol, text }: IButtonProps) => {
   return (
-    <div className="btn-container">
-      <button>
-        <span className="btn-symbol">{symbol}</span>
-        <span className="btn-text">{text}</span>
-      </button>
-    </div>
+    <>
+      <StyledButton>
+        <span>{symbol}</span>
+        <span>{text}</span>
+      </StyledButton>
+    </>
   );
 };
--- src/components/CodeExerciseApp.tsx
+++ src/components/CodeExerciseApp.tsx
@@ -1,15 +1,30 @@
 import { Button } from "./Button";
 import { Counter } from "./Counter";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1rem;
+`;
 
 export const CodeExerciseApp = () => {
   return (
-    <div>
+    <Wrapper>
       <p>NoA Ignite - code exercise</p>
-      <div className="container">
+      <ButtonsContainer>
         <Button symbol={"- "} text={"DECREMENT"}></Button>
         <Counter />
         <Button symbol={"+ "} text={"INCREMENT"}></Button>
-      </div>
-    </div>
+      </ButtonsContainer>
+    </Wrapper>
   );
 };
