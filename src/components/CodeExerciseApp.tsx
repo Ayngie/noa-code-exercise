2958b48bee6c5e51a473457d2a4cf023a9ca75cd
Ayngie <angelicareutersward@gmail.com>
2023-05-19 16:00:06 +0200

    ♻️ (refactor): refactored wrappers code, moved into separate styles file

--- src/components/CodeExerciseApp.tsx
+++ src/components/CodeExerciseApp.tsx
@@ -1,40 +1,18 @@
-import NormalButton from "./NormalButton";
+import { Button } from "../styles/Button";
+import { MainWrapper, ButtonsContainer } from "../styles/Wrappers";
 import { Counter } from "./Counter";
-import PrimaryButton from "./PrimaryButton";
-import styled from "styled-components";
 import { DisplayRepo } from "./DisplayRepo";
-
-const Wrapper = styled.div`
-  margin: 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 2rem;
-`;
-
-const ButtonsContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 1rem;
-`;
 
 export const CodeExerciseApp = () => {
   return (
-    <Wrapper>
+    <MainWrapper>
       <h1>NoA Ignite - code exercise</h1>
       <ButtonsContainer>
-        <NormalButton
-          symbol={"-"}
-          text={"DECREMENT"}
-          className={"normal-button"}></NormalButton>
+        <Button symbol={"-"} text={"DECREMENT"} className={"normal"}></Button>
         <Counter />
-        <PrimaryButton
-          symbol={"+"}
-          text={"INCREMENT"}
-          className={"primary-button"}></PrimaryButton>
+        <Button symbol={"+"} text={"INCREMENT"} className={"primary"}></Button>
       </ButtonsContainer>
       <DisplayRepo />
-    </Wrapper>
+    </MainWrapper>
   );
 };
---
+++ src/styles/Wrappers.tsx
@@ -0,0 +1,22 @@
+import { styled } from "styled-components";
+
+export const MainWrapper = styled.div`
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const ButtonContentsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
