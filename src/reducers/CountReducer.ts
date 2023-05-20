72c53fd05ac8577868d88e1360d4e40da93b5cca
Ayngie <angelicareutersward@gmail.com>
2023-05-20 14:54:15 +0200

    ✨ (feat): added logic for cases, added class as datatype for parameter variable

--- src/reducers/CountReducer.ts
+++ src/reducers/CountReducer.ts
@@ -1,3 +1,5 @@
+import { Count } from "../models/Count";
+
 export enum ActionType {
   INCREMENTED,
   DECREMENTED,
@@ -8,18 +10,28 @@ export interface IAction {
   payload: number;
 }
 
-export const CountReducer = (count: number, action: IAction) => {
+export const CountReducer = (currentCount: Count, action: IAction) => {
   switch (action.type) {
-    case ActionType.INCREMENTED:
-      if (action.payload < 8) return { ...count, count: action.payload + 1 };
-      else {
-        return count;
+    case ActionType.INCREMENTED: {
+      if (action.payload < 8) {
+        console.log("Clicked increment");
+        return { ...currentCount, count: action.payload + 1 };
+        // return count;
+      } else {
+        return currentCount;
       }
-    case ActionType.DECREMENTED:
-      if (action.payload > 0) return { ...count, count: action.payload - 1 };
-      else {
-        return count;
+    }
+
+    case ActionType.DECREMENTED: {
+      if (action.payload > 0) {
+        console.log("Clicked decrement");
+        return { ...currentCount, count: action.payload - 1 };
+        // return count;
+      } else {
+        return currentCount;
       }
+    }
+
     default:
       //   return count;
       throw Error("Type does not exist, check spelling");
