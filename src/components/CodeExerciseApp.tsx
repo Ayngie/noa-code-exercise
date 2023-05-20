0b30ac5361f37669d0d1e4ec4d4aebb6db866657
Ayngie <angelicareutersward@gmail.com>
2023-05-20 15:30:09 +0200

    ✨ (feat): added variable for currentRepo

--- src/components/CodeExerciseApp.tsx
+++ src/components/CodeExerciseApp.tsx
@@ -1,4 +1,4 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 import { ActionType, CountReducer } from "../reducers/CountReducer";
 import { Button } from "../styles/Button";
 import { MainWrapper, ButtonsContainer } from "../styles/Wrappers";
@@ -8,9 +8,26 @@ import { DisplayRepo } from "./DisplayRepo";
 export const CodeExerciseApp = () => {
   const [currentCount, dispatch] = useReducer(
     CountReducer,
-    JSON.parse(localStorage.getItem("currentCountFromLS") || "0")
+    JSON.parse(localStorage.getItem("currentCountFromLS") || "5")
   );
   localStorage.setItem("currentCountFromLS", JSON.stringify(currentCount));
+
+  const repoNamesList = [
+    { repoName: "eslint/eslint", position: 0 },
+    { repoName: "oakwood/front-end-questions", position: 1 },
+    { repoName: "babel/babel", position: 2 },
+    { repoName: "webpack/webpack", position: 3 },
+    { repoName: "storybooks/storybook", position: 4 },
+    { repoName: "facebook/react", position: 5 },
+    { repoName: "reactjs/redux", position: 6 },
+    { repoName: "expressjs/express", position: 7 },
+  ];
+  // console.log("List of repoNames: ", repoNamesList);
+  // console.log("repoNamesList[0]: ", repoNamesList[0]);
+  // console.log("repoNamesList[currentCount]: ", repoNamesList[+currentCount]);
+
+  const currentRepo = repoNamesList[+currentCount];
+  console.log("currentRepo: ", currentRepo);
 
   return (
     <MainWrapper>
