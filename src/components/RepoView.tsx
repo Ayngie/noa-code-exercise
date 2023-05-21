1aaf42c0001c2c2a8c6e441b49f971e9426de0c3
Ayngie <angelicareutersward@gmail.com>
2023-05-21 17:49:23 +0200

    ✨ (feat): added interface for props and interpolations of properties

--- src/components/RepoView.tsx
+++ src/components/RepoView.tsx
@@ -1,15 +1,21 @@
-export const RepoView = () => {
+import { IRepo } from "../models/IRepo";
+
+interface IRepoViewProps {
+  repoToShow: IRepo;
+}
+
+export const RepoView = ({ repoToShow }: IRepoViewProps) => {
   return (
     <div>
       <h2>The repository you chose is:</h2>
       <p>
-        <span>Full name:</span> <span></span>
+        <span>Full name:</span> <span>{repoToShow.full_name}</span>
       </p>
       <p>
-        <span>Description:</span> <span></span>
+        <span>Description:</span> <span>{repoToShow.description}</span>
       </p>
       <p>
-        <span>Amount of stars:</span> <span></span>
+        <span>Amount of stars:</span> <span>{repoToShow.stargazers_count}</span>
       </p>
     </div>
   );
