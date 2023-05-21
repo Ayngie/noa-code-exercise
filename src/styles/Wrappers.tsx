419f752aacf8e63a2fd30b7bbde7a05026b24a48
Ayngie <angelicareutersward@gmail.com>
2023-05-22 00:51:13 +0200

    💄 (style): updated responsive max-width, added styling to mainwrapper

--- src/styles/Wrappers.tsx
+++ src/styles/Wrappers.tsx
@@ -1,13 +1,14 @@
 import { styled } from "styled-components";
 
 export const MainWrapper = styled.div`
-  padding: 5%;
+  padding: 3rem;
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 2rem;
 
   background-color: #fff6eb;
+  border-radius: 10px;
 
   h1 {
     text-align: center;
@@ -18,7 +19,12 @@ export const CodeTestWrapper = styled(MainWrapper)`
   background-color: white;
   padding: 2rem;
   border: 5px solid blanchedalmond;
-  height: auto;
+  border-radius: 5px;
+
+  width: 60%;
+  @media (max-width: 1000px) {
+    width: 90%;
+  }
 `;
 
 export const CounterRow = styled.div`
@@ -27,7 +33,7 @@ export const CounterRow = styled.div`
   align-items: center;
   gap: 1rem;
 
-  @media (max-width: 768px) {
+  @media (max-width: 1000px) {
     flex-direction: column;
   }
 `;
@@ -46,7 +52,7 @@ export const RepositoryContents = styled.div`
   align-items: center;
 
   width: 60%;
-  @media (max-width: 768px) {
+  @media (max-width: 1000px) {
     width: 90%;
   }
 `;
