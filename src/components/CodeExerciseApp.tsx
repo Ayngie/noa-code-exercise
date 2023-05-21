f2a7b7c277e734d04afbfb7898d07060c08ae195
Ayngie <angelicareutersward@gmail.com>
2023-05-21 17:50:57 +0200

    ✨ (feat): added useEffect and function to getData. Moved  list and variable into useEffect

--- src/components/CodeExerciseApp.tsx
+++ src/components/CodeExerciseApp.tsx
@@ -1,28 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MainWrapper } from "../styles/Wrappers";
 import { CounterView } from "./CounterView";
 import { RepoView } from "./RepoView";
+import { IRepo } from "../models/IRepo";
+import { IError } from "../models/IError";
+import axios from "axios";
 
 export const CodeExerciseApp = () => {
-  const repoNamesList = [
-    { repoName: "eslint/eslint" },
-    { repoName: "oakwood/front-end-questions" },
-    { repoName: "babel/babel" },
-    { repoName: "webpack/webpack" },
-    { repoName: "storybooks/storybook" },
-    { repoName: "facebook/react" },
-    { repoName: "reactjs/redux" },
-    { repoName: "expressjs/express" },
-  ];
-
   // const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [count, setCount] = useState<number>(7);
+  const [count, setCount] = useState<number>(2);
   console.log("count:", count);
 
-  const [repoToGet, setRepoToGet] = useState<string>(
-    repoNamesList[count].repoName
-  );
-  console.log("repoToGet: ", repoToGet);
+  //variable for reponame:
+  //make sure variablename updates with state change of count
+
+  //call getRepos with variablename, catch response
+  const [repoToShow, setRepoToShow] = useState<IRepo>({
+    full_name: "",
+    description: "",
+    stargazers_count: 0,
+  });
+
+  useEffect(() => {
+    const repoNamesList = [
+      { repoName: "eslint/eslint" },
+      { repoName: "oakwood/front-end-questions" },
+      { repoName: "babel/babel" },
+      { repoName: "webpack/webpack" },
+      { repoName: "storybooks/storybook" },
+      { repoName: "facebook/react" },
+      { repoName: "reactjs/redux" },
+      { repoName: "expressjs/express" },
+    ];
+
+    let repoToGet: string = repoNamesList[count].repoName;
+    console.log("repoToGet: ", repoToGet);
+
+    async function fetchData() {
+      try {
+        // You can await here
+        const response = await axios.get<IRepo>(
+          `https://api.github.com/repos/${repoToGet}`
+        );
+        // ...
+        const newRepo: IRepo = {
+          full_name: response.data.full_name,
+          description: response.data.description,
+          stargazers_count: response.data.stargazers_count,
+        };
+        setRepoToShow(newRepo);
+      } catch (error) {
+        console.error("Oops, there was the following error: ", error);
+        return {};
+      }
+    }
+
+    fetchData();
+  }, [count]); // Or [] if effect doesn't need props or state
+
+  //send response as prop
 
   const handleIncrement = () => {
     console.log("Clicked increment");
@@ -30,10 +66,10 @@ export const CodeExerciseApp = () => {
     if (count < 7) {
       console.log("Incremented one.");
       let newCount: number = count + 1;
-      let nameOfNewRepoToGet: string = repoNamesList[newCount].repoName;
+      // let nameOfNewRepoToGet: string = repoNamesList[newCount].repoName;
 
       setCount(newCount);
-      setRepoToGet(nameOfNewRepoToGet);
+      // setRepoToGet(nameOfNewRepoToGet);
     } else {
       console.log("Count is 7, no increment action was performed.");
     }
@@ -45,10 +81,10 @@ export const CodeExerciseApp = () => {
     if (count > 0) {
       console.log("Decremented one.");
       let newCount: number = count - 1;
-      let nameOfNewRepoToGet: string = repoNamesList[newCount].repoName;
+      // let nameOfNewRepoToGet: string = repoNamesList[newCount].repoName;
 
       setCount(newCount);
-      setRepoToGet(nameOfNewRepoToGet);
+      // setRepoToGet(nameOfNewRepoToGet);
     } else {
       console.log("Count is 0, no decrement action was performed.");
     }
@@ -62,8 +98,8 @@ export const CodeExerciseApp = () => {
         handleIncrement={handleIncrement}
         handleDecrement={handleDecrement}
       />
-      {/* {isLoading && <h3>Repo is loading...</h3>} */}
-      <RepoView />
+      {/* {isLoading && <h3>Repository is loading...</h3>} */}
+      <RepoView repoToShow={repoToShow} />
     </MainWrapper>
   );
 };
