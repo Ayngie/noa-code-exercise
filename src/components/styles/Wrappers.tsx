fbd5c9c74200d3ce41101b5467eb06d305718846
Ayngie <angelicareutersward@gmail.com>
2023-05-25 13:32:25 +0200

    ♻️ (refactor): refactored wrapperscode to make it more DRY

--- src/components/styles/Wrappers.tsx
+++ src/components/styles/Wrappers.tsx
@@ -1,35 +1,33 @@
 import { styled } from "styled-components";
 
-export const MainWrapper = styled.div`
-  padding: 3rem;
+export const ColumnWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 2rem;
+`;
 
+export const MainWrapper = styled(ColumnWrapper)`
+  padding: 3rem;
+  gap: 1rem;
   h1 {
     text-align: center;
   }
 `;
 
-export const CodeTestWrapper = styled(MainWrapper)`
-  background-color: white;
+export const CodeTestWrapper = styled(ColumnWrapper)`
+  gap: 2rem;
   padding: 2rem;
+  background-color: white;
   border: 5px solid blanchedalmond;
   border-radius: 5px;
   width: 70%;
-
   @media (min-width: 1000px) {
     width: 60%;
   }
 `;
 
-export const CounterRowWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+export const CounterRowWrapper = styled(ColumnWrapper)`
   gap: 1rem;
-
   @media (min-width: 1000px) {
     flex-direction: row;
   }
@@ -37,24 +35,18 @@ export const CounterRowWrapper = styled.div`
 
 export const ButtonContentsWrapper = styled.div`
   display: flex;
+  flex-direction: row;
   align-items: center;
   gap: 0.5rem;
 `;
 
-export const RepositoryWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 90%;
-
+export const RepositoryWrapper = styled(ColumnWrapper)`
+  width: 100%;
   @media (min-width: 1000px) {
     width: 60%;
   }
 `;
 
-export const NotFoundWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+export const NotFoundWrapper = styled(ColumnWrapper)`
   text-align: center;
 `;
